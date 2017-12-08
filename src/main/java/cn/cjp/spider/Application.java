7315815bad95f8dfce2f97446ae90a0effd5e182
sucre <cuijinpeng@betago2016.com>
2017-12-08 14:00:01 +0800

    upd

--- src/main/java/cn/cjp/spider/Application.java
+++ src/main/java/cn/cjp/spider/Application.java
@@ -1,6 +1,7 @@
 package cn.cjp.spider;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
@@ -34,7 +35,7 @@ public class Application extends SpringBootServletInitializer {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+	    SpringApplication.run(Application.class, args);
 	}
 
 }
