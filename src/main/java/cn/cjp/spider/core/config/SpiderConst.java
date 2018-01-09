85fcf2cff7415f6a4839005241612198dbc7f1de
jpcui <624498030@qq.com>
2018-01-09 19:07:15 +0800

    涉及到的常量

---
+++ src/main/java/cn/cjp/spider/core/config/SpiderConst.java
@@ -0,0 +1,16 @@
+package cn.cjp.spider.core.config;
+
+/**
+ * 定义常量，包含key
+ */
+public class SpiderConst {
+
+    public static final String KEY_DB_NAME = "_db";
+
+    public static final String KEY_TABLE_NAME = "_tableName";
+
+    /**
+     * 来源
+     */
+    public static final String KEY_REFER_URL = "_refer";
+}
