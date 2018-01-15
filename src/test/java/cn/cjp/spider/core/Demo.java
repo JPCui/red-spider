5450cc51dd192dddee98420c65ceb5156b8cbded
jpcui <624498030@qq.com>
2018-01-15 09:36:21 +0800

    upd

--- src/test/java/cn/cjp/spider/core/Demo.java
+++ src/test/java/cn/cjp/spider/core/Demo.java
@@ -1,13 +1,24 @@
 package cn.cjp.spider.core;
 
-import java.util.UUID;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Demo {
 
     public static void main(String[] args) {
-        UUID uuid = UUID.nameUUIDFromBytes("asd".getBytes());
-        
-        System.out.println(UUID.nameUUIDFromBytes("asd".getBytes()));
+        List<String> list = new ArrayList<>();
+        list.add("b");
+        list.add("c");
+        list.add("a");
+        list.add("a");
+        list.add("c");
+
+        System.out.println(list);
+
+        System.out.println(list.stream().distinct().collect(Collectors.toList()));
+
+        System.out.println(list);
     }
 
 }
