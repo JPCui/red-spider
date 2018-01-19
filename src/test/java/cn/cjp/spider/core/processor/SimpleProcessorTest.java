e2c2a994a883544bcf303c2d69f109009bf414d4
jpcui <624498030@qq.com>
2018-01-19 21:03:50 +0800

    设置重试机制

--- src/test/java/cn/cjp/spider/core/processor/SimpleProcessorTest.java
+++ src/test/java/cn/cjp/spider/core/processor/SimpleProcessorTest.java
@@ -42,9 +42,9 @@ public class SimpleProcessorTest {
 			site.addHeader("User-Agent", UserAgents.get());
 			site.setDomain(siteModel.getSiteName());
 			site.setSleepTime(3000);
-			site.setRetrySleepTime(10_000); // 重试休息时间：10s
-			site.setRetryTimes(10); // 重试 10次
-			site.setTimeOut(30000); // 超时时间 30s
+			site.setRetrySleepTime(30_000); // 重试休息时间：10s
+			site.setRetryTimes(5); // 重试 10次
+			site.setTimeOut(30_000); // 超时时间 30s
 			simpleProcessor.setSite(site);
 
 			try {
