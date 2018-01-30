4fc7e2d2fa7c0a3caa8a874f134d8ac7c459629d
jpcui <624498030@qq.com>
2018-01-30 23:01:39 +0800

    refactor

--- src/main/java/cn/cjp/SpiderAppApplication.java
+++ src/main/java/cn/cjp/SpiderAppApplication.java
@@ -12,25 +12,25 @@ import org.springframework.core.task.SimpleAsyncTaskExecutor;
 @ServletComponentScan
 public class SpiderAppApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpiderAppApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpiderAppApplication.class, args);
+	}
 
-    /**
-     * 创建异步任务执行器
-     */
-    @Bean
-    public SimpleAsyncTaskExecutor simpleAsyncTaskExecutor() {
-        SimpleAsyncTaskExecutor simpleAsyncTaskExecutor = new SimpleAsyncTaskExecutor();
-        simpleAsyncTaskExecutor.setThreadNamePrefix("Async-Task-");
-        simpleAsyncTaskExecutor.setDaemon(true);
-        simpleAsyncTaskExecutor.setConcurrencyLimit(10);
-        return simpleAsyncTaskExecutor;
-    }
+	/**
+	 * 创建异步任务执行器
+	 */
+	@Bean
+	public SimpleAsyncTaskExecutor simpleAsyncTaskExecutor() {
+		SimpleAsyncTaskExecutor simpleAsyncTaskExecutor = new SimpleAsyncTaskExecutor();
+		simpleAsyncTaskExecutor.setThreadNamePrefix("Async-Task-");
+		simpleAsyncTaskExecutor.setDaemon(true);
+		simpleAsyncTaskExecutor.setConcurrencyLimit(10);
+		return simpleAsyncTaskExecutor;
+	}
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SpiderAppApplication.class);
-    }
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpiderAppApplication.class);
+	}
 
 }
