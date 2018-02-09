b90a7f7f64eb450c212e7daf13c5b7bbdcb54b03
jpcui <624498030@qq.com>
2018-02-09 21:59:14 +0800

    text

---
+++ src/test/java/cn/cjp/app/service/ReaderRecordServiceTest.java
@@ -0,0 +1,38 @@
+package cn.cjp.app.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import cn.cjp.SpiderAppApplication;
+import cn.cjp.app.model.doc.ReaderRecord;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@SpringBootTest(classes = SpiderAppApplication.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class ReaderRecordServiceTest {
+
+	@Autowired
+	ReaderRecordService readerRecordService;
+
+	String userId = "";
+
+	@Test
+	public void test() {
+		String bookDocId = "bookDocId";
+		int index = 1;
+		readerRecordService.save(userId, bookDocId, index);
+
+		ReaderRecord readerRecord = readerRecordService.findOne(userId);
+
+		Assert.assertNotNull(readerRecord);
+		Assert.assertEquals(bookDocId, readerRecord.getBookDocId());
+		Assert.assertEquals(index, readerRecord.getIndex());
+
+	}
+
+}
