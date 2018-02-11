60256c851973ea9f4a4f3fcb824910336702ed0f
jpcui <624498030@qq.com>
2018-02-11 19:01:56 +0800

    transfer between mongo doc and entity

---
+++ src/main/java/cn/cjp/app/model/doc/DocUtil.java
@@ -0,0 +1,89 @@
+package cn.cjp.app.model.doc;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+public class DocUtil {
+
+    /**
+     * 把实体bean对象转换成DBObject
+     */
+    public static <T> DBObject bean2DBObject(T bean) throws IllegalAccessException {
+        if (bean == null) {
+            return null;
+        }
+        DBObject dbObject = new BasicDBObject();
+        // 获取对象对应类中的所有属性域
+        List<Field> fieldList = new ArrayList<>();
+        Class tempClass = bean.getClass();
+        while (tempClass != null) {//当父类为null的时候说明到达了最上层的父类(Object类).
+            fieldList.addAll(Arrays.asList(tempClass.getDeclaredFields()));
+            tempClass = tempClass.getSuperclass(); //得到父类,然后赋给自己
+        }
+        for (Field field : fieldList) {
+            // 获取属性名
+            String varName = field.getName();
+            // 修改访问控制权限
+            boolean accessFlag = field.isAccessible();
+            if (!accessFlag) {
+                field.setAccessible(true);
+            }
+            Object param = field.get(bean);
+            if (param == null) {
+                continue;
+            } else if (param instanceof Integer) {//判断变量的类型
+                int value = ((Integer) param).intValue();
+                dbObject.put(varName, value);
+            } else if (param instanceof String) {
+                String value = (String) param;
+                dbObject.put(varName, value);
+            } else if (param instanceof Double) {
+                double value = ((Double) param).doubleValue();
+                dbObject.put(varName, value);
+            } else if (param instanceof Float) {
+                float value = ((Float) param).floatValue();
+                dbObject.put(varName, value);
+            } else if (param instanceof Long) {
+                long value = ((Long) param).longValue();
+                dbObject.put(varName, value);
+            } else if (param instanceof Boolean) {
+                boolean value = ((Boolean) param).booleanValue();
+                dbObject.put(varName, value);
+            } else if (param instanceof Date) {
+                Date value = (Date) param;
+                dbObject.put(varName, value);
+            }
+            // 恢复访问控制权限
+            field.setAccessible(accessFlag);
+        }
+        return dbObject;
+    }
+
+    /**
+     * 把DBObject转换成bean对象
+     */
+    public static <T> T dbObject2Bean(DBObject dbObject, T bean) throws IllegalAccessException,
+        InvocationTargetException, NoSuchMethodException {
+        if (bean == null) {
+            return null;
+        }
+        Field[] fields = bean.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            String varName = field.getName();
+            Object object = dbObject.get(varName);
+            if (object != null) {
+                BeanUtils.setProperty(bean, varName, object);
+            }
+        }
+        return bean;
+    }
+}  
