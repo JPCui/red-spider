bdc34f14723dd14f5b232ee06ada2a70260964a5
jpcui <624498030@qq.com>
2018-02-11 14:00:13 +0800

    clear

--- src/main/java/cn/cjp/app/config/Symphony.java
+++ src/main/java/cn/cjp/app/config/Symphony.java
@@ -1,16 +1,11 @@
 package cn.cjp.app.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import lombok.Data;
 
 @Data
-@Configuration
 @ConfigurationProperties(prefix = "symphony", ignoreUnknownFields = true)
-@ConditionalOnProperty("symphony.enable")
 public class Symphony {
 
     private boolean enable;
